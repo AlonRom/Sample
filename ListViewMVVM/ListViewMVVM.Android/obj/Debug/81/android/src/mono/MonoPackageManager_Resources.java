0a9f2fad03a009265b94ab9bfb543c79363a3a8f
Alon Rom <romalon1@gmail.com>
2020-07-13 08:18:06 +0300

    First commit

---
+++ ListViewMVVM/ListViewMVVM.Android/obj/Debug/81/android/src/mono/MonoPackageManager_Resources.java
@@ -0,0 +1,43 @@
+package mono;
+public class MonoPackageManager_Resources {
+	public static String[] Assemblies = new String[]{
+		/* We need to ensure that "ListViewMVVM.Android.dll" comes first in this list. */
+		"ListViewMVVM.Android.dll",
+		"FormsViewGroup.dll",
+		"ListViewMVVM.dll",
+		"Sharpnado.Shadows.Android.dll",
+		"Sharpnado.Shadows.dll",
+		"Syncfusion.Core.XForms.Android.dll",
+		"Syncfusion.Core.XForms.dll",
+		"Syncfusion.DataSource.Portable.dll",
+		"Syncfusion.GridCommon.Portable.dll",
+		"Syncfusion.Licensing.dll",
+		"Syncfusion.SfListView.XForms.Android.dll",
+		"Syncfusion.SfListView.XForms.dll",
+		"Xamarin.Android.Arch.Core.Common.dll",
+		"Xamarin.Android.Arch.Lifecycle.Common.dll",
+		"Xamarin.Android.Arch.Lifecycle.Runtime.dll",
+		"Xamarin.Android.Support.Animated.Vector.Drawable.dll",
+		"Xamarin.Android.Support.Annotations.dll",
+		"Xamarin.Android.Support.Compat.dll",
+		"Xamarin.Android.Support.Core.UI.dll",
+		"Xamarin.Android.Support.Core.Utils.dll",
+		"Xamarin.Android.Support.CustomTabs.dll",
+		"Xamarin.Android.Support.Design.dll",
+		"Xamarin.Android.Support.Fragment.dll",
+		"Xamarin.Android.Support.Media.Compat.dll",
+		"Xamarin.Android.Support.Transition.dll",
+		"Xamarin.Android.Support.v4.dll",
+		"Xamarin.Android.Support.v7.AppCompat.dll",
+		"Xamarin.Android.Support.v7.CardView.dll",
+		"Xamarin.Android.Support.v7.RecyclerView.dll",
+		"Xamarin.Android.Support.Vector.Drawable.dll",
+		"Xamarin.Forms.Core.dll",
+		"Xamarin.Forms.Platform.Android.dll",
+		"Xamarin.Forms.Platform.dll",
+		"Xamarin.Forms.Xaml.dll",
+	};
+	public static String[] Dependencies = new String[]{
+	};
+	public static String ApiPackageName = "Mono.Android.Platform.ApiLevel_27";
+}
